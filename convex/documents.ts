3c64fdfab8fa2ed31ccb6ddbd1bf5fa36cb8f99e
masud-rana44 <mdmasudrana4488@gmail.com>
2025-01-17 16:00:36 +0600

    Setting up prompt

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,16 +1,84 @@
+import OpenAI from "openai";
+import { api } from "./_generated/api";
 import { ConvexError, v } from "convex/values";
 import { action, mutation, query } from "./_generated/server";
-import { api } from "./_generated/api";
-import OpenAI from "openai";
+import mammoth from "mammoth";
+import pdfParse from "pdf-parse";
+import XLSX from "xlsx";
+// import pptxParser from "pptx-parser";
+import { fileTypeFromBuffer } from "file-type";
 
 const modelName = "gpt-4o";
 const token = process.env.GITHUB_TOKEN;
+const maxTokenLength = 6000;
 const endpoint = "https://models.inference.ai.azure.com";
 
 const client = new OpenAI({
   baseURL: endpoint,
   apiKey: token,
 });
+
+export interface FileWithMetadata extends Blob {
+  fileName?: string;
+  contentType?: string;
+}
+
+export async function extractTextFromFile(
+  file: FileWithMetadata
+): Promise<string> {
+  const fileBuffer = Buffer.from(await file.arrayBuffer());
+  const detectedFileType = await fileTypeFromBuffer(fileBuffer);
+  const fileType =
+    detectedFileType?.ext ||
+    file.contentType ||
+    file.fileName?.split(".").pop();
+
+  let extractedText = "";
+
+  try {
+    switch (fileType) {
+      case "pdf":
+        const pdfData = await pdfParse(fileBuffer);
+        extractedText = pdfData.text;
+        console.log({ extractedText });
+        break;
+
+      case "docx":
+        const docxData = await mammoth.extractRawText({ buffer: fileBuffer });
+        extractedText = docxData.value;
+        break;
+
+      // TODO: Add support for doc
+
+      case "xlsx":
+      case "xls":
+        const workbook = XLSX.read(fileBuffer, { type: "buffer" });
+        workbook.SheetNames.forEach((sheet) => {
+          const rows = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet]);
+          extractedText += rows;
+        });
+        break;
+
+      // case "pptx":
+      //   const sliders = await pptxParser(fileBuffer);
+      //   sliders.forEach((slide) => {
+      //     extractedText += slide.text;
+      //   });
+      //   break;
+
+      case "txt":
+        extractedText = fileBuffer.toString("utf-8");
+
+      default:
+        throw new Error(`Unsupported file type: ${fileType}`);
+    }
+  } catch (error) {
+    console.log("Error extracting text: ", error);
+    throw new Error("Failed to extract text from the file");
+  }
+
+  return extractedText;
+}
 
 export const generateUploadUrl = mutation({
   handler: async (ctx) => {
@@ -85,22 +153,25 @@ export const aksQuestion = action({
     if (!file) throw new ConvexError("File not found");
 
     const text = await file.text();
-
-    console.log({ text });
+    const truncatedText = text.slice(0, maxTokenLength);
 
     const chatCompletion: OpenAI.Chat.Completions.ChatCompletion =
       await client.chat.completions.create({
         messages: [
-          { role: "system", content: `Here is a text file: ${text}` },
+          {
+            role: "system",
+            content:
+              "You area an AI Assistant specialized in analyzing text documents. Provide accurate answers based only on the provided text.",
+          },
           {
             role: "user",
-            content: `Please answer the question ${args.question}`,
+            content: `Here is the text document: ${truncatedText}\n\nAnswer this question: ${args.question}. If the answer isn't in the text, reply, "The answer is not provided in the text.`,
           },
         ],
         model: modelName,
-        // temperature: 1,
-        // max_tokens: 4096,
-        // top_p: 1,
+        temperature: 0.3,
+        max_tokens: maxTokenLength,
+        top_p: 1,
       });
 
     return chatCompletion.choices[0].message.content;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,53 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import mammoth from "mammoth";
-import pdfParse from "pdf-parse";
-import XLSX from "xlsx";
-import pptxParser from "pptx-parser";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export interface UploadFile {
-  arrayBuffer: () => Promise<ArrayBuffer>;
-  contentType?: string;
-  fileName: string;
-}
-
-export async function extractTextFromFile(file: UploadFile): Promise<string> {
-  const fileBuffer = Buffer.from(await file.arrayBuffer());
-  const fileType = file.contentType || file.fileName.split(".").pop();
-
-  let extractedText = "";
-
-  try {
-    switch (fileType) {
-      case "pdf":
-        const pdfData = await pdfParse(fileBuffer);
-        extractedText = pdfData.text;
-        break;
-
-      case "docx":
-        const docxData = await mammoth.extractRawText({ buffer: fileBuffer });
-        extractedText = docxData.value;
-        break;
-
-      case "xlsx":
-      case "xls":
-        const workbook = XLSX.read(fileBuffer, { type: "buffer" });
-        workbook.SheetNames.forEach((sheet) => {
-          const rows = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet]);
-          extractedText += rows;
-        });
-        break;
-
-      case "pptx":
-        const sliders = await pptxParser(fileBuffer);
-        sliders.forEach((slide) => {
-          extractedText += slide.text;
-        });
-        break;
-    }
-  } catch {}
 }
