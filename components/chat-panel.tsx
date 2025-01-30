c4dac31eebc3922b27913884d343bdab07c09a8f
masud-rana44 <mdmasudrana4488@gmail.com>
2025-01-30 12:53:42 +0600

    Improve ux on chat

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -33,7 +33,7 @@ export default function ChatPanel({
   return (
     <div className="h-full flex flex-col gap-4">
       <div className="flex-1 overflow-y-auto flex flex-col space-y-3">
-        <div className="bg-slate-900 p-3 flex gap-3">
+        <div className="bg-slate-950 p-3 flex gap-3">
           <span>
             <Avatar isHuman={false} imgUrl={user.user?.imageUrl} />
           </span>
@@ -43,16 +43,13 @@ export default function ChatPanel({
         </div>
         {/* If not chats, added loading skeleton */}
         {!chats &&
-          Array.from({ length: 6 }).map((_, idx) => (
+          Array.from({ length: 4 }).map((_, idx) => (
             <Skeleton
               key={idx}
-              className={cn(
-                {
-                  "bg-slate-800 self-end py-2 px-3 h-8 ": idx % 2 == 0,
-                  "bg-slate-950 p-4 h-20": idx % 2 == 1,
-                },
-                "w-2/3"
-              )}
+              className={cn({
+                "bg-slate-800 self-end py-2 px-3 h-8 w-1/3": idx % 2 == 0,
+                "bg-slate-950 p-4 h-32 w-full": idx % 2 == 1,
+              })}
             />
           ))}
 
@@ -62,9 +59,9 @@ export default function ChatPanel({
               key={chat._id}
               className={cn(
                 {
-                  "bg-slate-700 text-right w-fit self-end py-2 px-3":
+                  "bg-slate-800 text-right w-fit self-end py-2 px-3":
                     chat.isHuman,
-                  "bg-slate-800 p-4": !chat.isHuman,
+                  "bg-slate-950 p-4": !chat.isHuman,
                 },
                 "rounded whitespace-pre-line flex items-start gap-2"
               )}
