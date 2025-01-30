9331dd20d2471671a92d4e5bf1b885675bec5bbf
masud-rana44 <mdmasudrana4488@gmail.com>
2025-01-30 12:41:20 +0600

    Enhance user experience on chat

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { useQuery } from "convex/react";
@@ -7,6 +7,7 @@ import { useUser } from "@clerk/clerk-react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Skeleton } from "./ui/skeleton";
+import Markdown from "react-markdown";
 
 export default function ChatPanel({
   documentId,
@@ -14,31 +15,28 @@ export default function ChatPanel({
   documentId: Id<"documents">;
 }) {
   const user = useUser();
+  const messageEndRef = useRef<HTMLDivElement>(null);
   const chats = useQuery(api.chats.getChatsForDocument, {
     documentId,
   });
 
-  const avatar = (isHuman: boolean) => {
-    return isHuman ? (
-      <Image
-        height={24}
-        width={24}
-        className="rounded-full"
-        src={user.user?.imageUrl || "/placeholder-avatar.png"}
-        alt={`${user.user?.firstName} ${user.user?.lastName}`}
-      />
-    ) : (
-      <div className="bg-slate-200 rounded-full p-1">
-        <Image src="/ai-icon.png" width={20} height={20} alt="AI Icon" />
-      </div>
-    );
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
   };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chats]);
 
   return (
     <div className="h-full flex flex-col gap-4">
       <div className="flex-1 overflow-y-auto flex flex-col space-y-3">
         <div className="bg-slate-900 p-3 flex gap-3">
-          <span>{avatar(false)}</span>
+          <span>
+            <Avatar isHuman={false} imgUrl={user.user?.imageUrl} />
+          </span>
           <p className="flex-1">
             Ask any question using AI about this document:
           </p>
@@ -64,21 +62,46 @@ export default function ChatPanel({
               key={chat._id}
               className={cn(
                 {
-                  "bg-slate-800 text-right w-fit self-end py-2 px-3":
+                  "bg-slate-700 text-right w-fit self-end py-2 px-3":
                     chat.isHuman,
-                  "bg-slate-950 p-4": !chat.isHuman,
+                  "bg-slate-800 p-4": !chat.isHuman,
                 },
                 "rounded whitespace-pre-line flex items-start gap-2"
               )}
             >
-              {!chat.isHuman && avatar(false)}
-              <span className="flex-1">{chat.text}</span>
-              {chat.isHuman && avatar(true)}
+              {!chat.isHuman && (
+                <Avatar isHuman={false} imgUrl={user.user?.imageUrl} />
+              )}
+              <Markdown className="flex-1">{chat.text}</Markdown>
+              {chat.isHuman && <Avatar isHuman imgUrl={user.user?.imageUrl} />}
             </div>
           ))}
+        <div ref={messageEndRef} />
       </div>
 
       <QuestionForm documentId={documentId} />
     </div>
   );
 }
+
+const Avatar = ({
+  isHuman,
+  imgUrl,
+}: {
+  isHuman: boolean;
+  imgUrl: string | undefined;
+}) => {
+  return isHuman ? (
+    <Image
+      height={24}
+      width={24}
+      className="rounded-full"
+      src={imgUrl || "/placeholder-avatar.png"}
+      alt="Image of AI Avatar or User"
+    />
+  ) : (
+    <div className="bg-slate-200 rounded-full p-1">
+      <Image src="/ai-icon.png" width={20} height={20} alt="AI Icon" />
+    </div>
+  );
+};
