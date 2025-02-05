be26277a7842af1f64362ff7fc537d769f2589d3
masud-rana44 <mdmasudrana4488@gmail.com>
2025-02-05 17:17:56 +0600

    Fix issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import { Toaster } from "@/components/ui/toaster";
+import { Providers } from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
