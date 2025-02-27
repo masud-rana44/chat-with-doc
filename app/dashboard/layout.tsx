49349f328f735b80df3822246c0bbcd5615fe750
masud-rana44 <mdmasudrana4488@gmail.com>
2025-02-27 10:02:51 +0600

    Dashboard layout updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from "react";
 import AppSidebar from "@/components/app-sidebar";
+import { useSidebar } from "@/components/sidebar-context";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const { isOpen, onClose } = useSidebar();
+
   return (
     <div className="flex">
       <AppSidebar />
 
-      <main className="mx-auto flex-1 p-6 lg:p-10 2xl:px-16">{children}</main>
+      <main
+        onClick={() => isOpen && onClose()}
+        className="mx-auto flex-1 p-6 lg:p-10 2xl:px-16"
+      >
+        {children}
+      </main>
     </div>
   );
 }
